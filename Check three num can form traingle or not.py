87d2aae8094b7d3dc29e87dda164bda550fafe52
keshab3736 <150231093+keshab3736@users.noreply.github.com>
2023-11-20 19:22:10 +0930

    Update Check three num can form traingle or not.py

--- Check three num can form traingle or not.py
+++ Check three num can form traingle or not.py
@@ -3,13 +3,11 @@ def is_triangle(a, b, c):
         return False
     else:
         return True
-
-# Take a side of triangle from user
+        
 side1 = float(input("Enter the first side: "))
 side2 = float(input("Enter the second side: "))
 side3 = float(input("Enter the third side: "))
 
-# Check if the sides can form a triangle or not
 if is_triangle(side1, side2, side3):
     print("Yes, the sides can form a triangle.")
 else:
